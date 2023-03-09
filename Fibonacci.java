80f98f979e7c3375523d87c5fbbfd2a7c962714f
Carlos Gabriel Reinoso Díaz <cualquieruncorreo@gmail.com>
2023-03-09 04:30:45 +0100

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,24 +2,24 @@ package fibonacci;
 
 public class Fibonacci {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		long a1=1, a2=1;
-		int numero=0;
-		
-		for (int i=1;  i<=46; i++) {
-			
-			numero++;
-			System.out.println("(" +numero +")" +a1);
-			numero++;
-			System.out.println("(" +numero+")" +a2);
-			
-			a1 = a1 + a2;
-			a2 = a1 + a2;
-			
-		}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
+        long a1 = 1, a2 = 1;
+        int numero = 0;
+
+        for (int i = 1; i <= 46; i++) {
+
+            numero++;
+            System.out.println("(" + numero + ")" + a1);
+            numero++;
+            System.out.println("(" + numero + ")" + a2);
+
+            a1 = a1 + a2;
+            a2 = a1 + a2;
+
+        }
+
+    }
 
 }
